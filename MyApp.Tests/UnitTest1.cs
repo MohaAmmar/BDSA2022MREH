688f8cd3c340840f5dc71d3698cc2899ab4ab561
mohammadammar <mreh@itu.dk>
2022-09-06 20:16:52 +0200

    Tested and implemented IsLeapYear

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+using System.IO;
+
+namespace MyApp.Tests;
+
+public class UnitTest1{
+    
+
+    [Fact]
+    public void Test1(){
+        var actual = "Jonas";
+        var expected = Program.printName("Jonas");
+
+        Assert.Equal(actual, expected);
+    }
+
+    [Fact]
+    public void ISLEAPYEAR_GIVEN_2002_FALSE()
+    {
+        Assert.False(Program.isLeapYear(2002));
+    }
+
+    [Fact]
+    public void ISLEAPYEAR_GIVEN_1600_TRUE()
+    {
+        Assert.True(Program.isLeapYear(1600));
+    }
+
+    [Fact]
+    public void ISLEAPYEAR_GIVEN_1700_FALSE()
+    {
+        Assert.False(Program.isLeapYear(1700));
+    }
+    [Fact]
+    public void ISLEAPYEAR_GIVEN_2004_TRUE()
+    {
+        Assert.True(Program.isLeapYear(2004));
+    }
+
+
+
+
+   
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            printName("Jonas");
+        }
+
+        public static string printName(string name){
+            return name;
+        }
+
+        public static bool isLeapYear(int year){
+            if((year % 400) == 0){
+                return true;
+            }else if((year % 100) == 0){
+                return false;
+            }else if((year % 4) == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+    }
+
+    
+}
+
